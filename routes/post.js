7c78cf2564ed295d91780261d12c82af96b076c2
h662 <godh662@gmail.com>
2020-04-04 16:10:13 +0900

    complete

--- routes/post.js
+++ routes/post.js
@@ -3,8 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-// const { Post, Hashtag, User } = require("../models");
-const { Post, User } = require("../models");
+const { Post, Hashtag, User } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
@@ -12,6 +11,7 @@ const router = express.Router();
 fs.readdir("uploads", error => {
   if (error) {
     console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+    fs.mkdirSync("uploads");
   }
 });
 
@@ -45,23 +45,45 @@ router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
       img: req.body.url,
       userId: req.user.id
     });
-    // const hashtags = req.body.content.match(/#[^\s]*/g);
-    // if (hashtags) {
-    //   const result = await Promise.all(
-    //     hashtags.map(tag =>
-    //       Hashtag.findOrCreate({
-    //         where: {
-    //           title: tag.slice(1).toLowerCase()
-    //         }
-    //       })
-    //     )
-    //   );
-    //   await post.addHashtags(result.map(r => r[0]));
-    // }
+    const hashtags = req.body.content.match(/#[^\s]*/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag =>
+          Hashtag.findOrCreate({
+            where: {
+              title: tag.slice(1).toLowerCase()
+            }
+          })
+        )
+      );
+      await post.addHashtags(result.map(r => r[0]));
+    }
     res.redirect("/");
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+router.get("/hashtag", async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect("/");
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+    return res.render("main", {
+      title: `${query} | NodeBird`,
+      user: req.user,
+      twits: posts
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 
